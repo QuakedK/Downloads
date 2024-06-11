4575a9ee426a3c8fa3eafc42fd0c589425622789
Quaked <124531365+QuakedK@users.noreply.github.com>
2024-06-10 19:30:53 -0500

    Update Winkey Afterrun.bat

--- Winkey Afterrun.bat
+++ Winkey Afterrun.bat
@@ -5,6 +5,12 @@ color 9
 set "HIVE=SYSTEM\CurrentControlSet"
 reg.exe add "HKLM\%HIVE%\Services\sppsvc" /v "Start" /t REG_DWORD /d "4" /f
 reg.exe add "HKLM\%HIVE%\Services\UsoSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg.exe add "HKLM\%HIVE%\Services\BITS" /v "Start" /t REG_DWORD /d "4" /f
+reg.exe add "HKLM\%HIVE%\Services\WaaSMedicSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg.exe add "HKLM\%HIVE%\Services\wlidsvc" /v "Start" /t REG_DWORD /d "4" /f
+reg.exe add "HKLM\%HIVE%\Services\LicenseManager" /v "Start" /t REG_DWORD /d "4" /f
+reg.exe add "HKLM\%HIVE%\Services\DoSvc" /v "Start" /t REG_DWORD /d "4" /f
+reg.exe add "HKLM\%HIVE%\Services\wuauserv" /v "Start" /t REG_DWORD /d "4" /f
 echo Services successfully disabled!
 timeout 2 > nul
    
