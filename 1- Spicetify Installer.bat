d44a1b73bea9836acc43881a66f3e0adbd285dc1
Quaked <124531365+QuakedK@users.noreply.github.com>
2024-06-16 10:20:07 -0500

    Delete 1- Spicetify Installer.bat

--- 1- Spicetify Installer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Running Spicetify installation...
-powershell -Command "iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex"
-echo Spicetify installation complete.
-pause
